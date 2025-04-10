aeb1f69556ddc2b310f247ba4962235056a3d385
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-09 22:11:00 -0300

    creat file main and add code

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import time
+import random
+from controller import pegar_pokemon, add_pokemon_to_db
+
+def main():
+    while True:
+        pokemon_id = random.randint(1, 350) # Gera ID aleatório de 1 até 350
+        pokemon_schema = pegar_pokemon(pokemon_id)
+        if pokemon_schema:
+            print(f"Adcionando {pokemon_schema.name} ao banco de dados.")
+            add_pokemon_to_db(pokemon_schema)
+        else:
+            print(f"Não foi possível obter dados para o Pokémon com ID {pokemon_schema.name}")
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
