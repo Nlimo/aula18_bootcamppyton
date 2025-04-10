f56886d6e02afd5b8688b416c21d57d7be8d7f34
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-09 21:30:06 -0300

    creat file and add code of models sqlalchemy

---
+++ src/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Interger, String, DateTime
+from sqlalchemy.sql import func
+from db import Base
+
+class Pokemon(Base):
+    __tablename__ = 'pokemons'
+
+    id = Column(Interger, primary_key=True, index=True)
+    name = Column(String)
+    type = Column(String)
+    creat_at = Column(DateTime, default=func.now()) # Campo add
